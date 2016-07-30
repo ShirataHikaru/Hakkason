40c8cf9e13a56ac5ffb5a6d06488ca4aee7ca274
ShirataHikaru <dendenden00@gmail.com>
2016-07-30 22:39:08 +0900

    storyboardの改変をしました

--- Hakkason/DrawingViewController.swift
+++ Hakkason/DrawingViewController.swift
@@ -178,61 +178,7 @@ class DrawingViewController: UIViewController, UIImagePickerControllerDelegate, 
             self.updateimg(data!)
         }
     }
-    @IBAction func send(sender: UIBarButtonItem) {
-        let sendimg =  self.drawingView.image
-        let transimg = UIImagePNGRepresentation(sendimg!)
-        var error : NSError?
-        do {
-            try self.session.sendData(transimg!,
-                                      toPeers: self.session.connectedPeers,
-                                      withMode: MCSessionSendDataMode.Unreliable)
-        } catch {
-            // do something.
-        }
-        
-        if error != nil {
-            print("Error sending data: \(error?.localizedDescription)")
-        }
 
-
-    }
-    func updateimg(recimg:NSData){
-        let chatchimage : UIImage! = UIImage(data:recimg)
-        let imgView = UIImageView(image:chatchimage)
-        self.drawingView.drawMode = ACEDrawingMode.Scale
-        self.drawingView.loadImage(chatchimage)
-    }
-    internal func OnClickSerchButton(sender:UIButton){
-         self.presentViewController(self.browser, animated: true, completion: nil)
-    }
-   
-    func browserViewControllerDidFinish(
-        browserViewController: MCBrowserViewController!)  {
-        // Called when the browser view controller is dismissed (ie the Done
-        // button was tapped)
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func browserViewControllerWasCancelled(
-        browserViewController: MCBrowserViewController!)  {
-        // Called when the browser view controller is cancelled
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func session(session: MCSession!, didReceiveData data: NSData!,
-                 fromPeer peerID: MCPeerID!)  {
-        // Called when a peer sends an NSData to us
-        
-        // This needs to run on the main queue
-        dispatch_async(dispatch_get_main_queue()) {
-            
-            
-            
-            self.updateimg(data!)
-        }
-    }
     
     // The following methods do nothing, but the MCSessionDelegate protocol
     // requires that we implement them.
@@ -260,50 +206,6 @@ class DrawingViewController: UIViewController, UIImagePickerControllerDelegate, 
         // Called when a connected peer changes state (for example, goes offline)
         
     }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // The following methods do nothing, but the MCSessionDelegate protocol
-    // requires that we implement them.
-    func session(session: MCSession!,
-                 didStartReceivingResourceWithName resourceName: String!,
-                                                   fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)  {
-        
-        // Called when a peer starts sending a file to us
-    }
-    
-    func session(session: MCSession!,
-                 didFinishReceivingResourceWithName resourceName: String!,
-                                                    fromPeer peerID: MCPeerID!,
-                                                             atURL localURL: NSURL!, withError error: NSError!)  {
-        // Called when a file has finished transferring from another peer
-    }
-    
-    func session(session: MCSession!, didReceiveStream stream: NSInputStream!,
-                 withName streamName: String!, fromPeer peerID: MCPeerID!)  {
-        // Called when a peer establishes a stream with us
-    }
-    
-    func session(session: MCSession!, peer peerID: MCPeerID!,
-                 didChangeState state: MCSessionState)  {
-        // Called when a connected peer changes state (for example, goes offline)
-        
-    }
-
-    
-    
-    
-    
-    
-    
-    
     
     
     override func didReceiveMemoryWarning() {
