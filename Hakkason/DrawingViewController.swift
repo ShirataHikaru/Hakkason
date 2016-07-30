b1c0896bbe0d17375211538afcd3324507a1458f
helloooooo <yomicerisier@gmail.com>
2016-07-30 15:57:07 +0900

    通信機能実装しました

--- Hakkason/DrawingViewController.swift
+++ Hakkason/DrawingViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 import ACEDrawingView
+import MultipeerConnectivity
 
-class DrawingViewController: UIViewController {
+class DrawingViewController: UIViewController,MCBrowserViewControllerDelegate,MCSessionDelegate {
     
     let ad = UIApplication.sharedApplication().delegate as! AppDelegate
-    
+    let serviceType = "LCOC-Chat"
+    var browser : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var peerID: MCPeerID!
+
     //描画画面をアウトレット接続してあります。
     @IBOutlet weak var drawingView: ACEDrawingView!
+    var SerchButton: UIBarButtonItem!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        SerchButton = UIBarButtonItem(title:"検索",style: .Plain,target:self,action: "OnClickSerchButton:")
+        self.navigationItem.leftBarButtonItem = SerchButton
         
+        self.peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        self.session = MCSession(peer: peerID)
+        self.session.delegate = self
+        self.browser = MCBrowserViewController(serviceType:serviceType,
+                                               session:self.session)
+        self.browser.delegate = self;
+        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
+                                               discoveryInfo:nil, session:self.session)
+        self.assistant.start()
+         navigationItem.backBarButtonItem?.accessibilityElementsHidden = true
+        
+    
         //線の太さの変更はこのように行います。
         drawingView.lineWidth = 2.0
 
@@ -55,6 +76,97 @@ class DrawingViewController: UIViewController {
     internal func onChangeWidthValueSlider(sender : UISlider){
         drawingView.lineWidth = CGFloat(sender.value)
     }
+    @IBAction func send(sender: UIBarButtonItem) {
+        let sendimg =  self.drawingView.image
+        let transimg = UIImagePNGRepresentation(sendimg!)
+        var error : NSError?
+        do {
+            try self.session.sendData(transimg!,
+                                      toPeers: self.session.connectedPeers,
+                                      withMode: MCSessionSendDataMode.Unreliable)
+        } catch {
+            // do something.
+        }
+        
+        if error != nil {
+            print("Error sending data: \(error?.localizedDescription)")
+        }
+
+
+    }
+    func updateimg(recimg:NSData){
+        let chatchimage : UIImage! = UIImage(data:recimg)
+        let imgView = UIImageView(image:chatchimage)
+        self.drawingView.drawMode = ACEDrawingMode.Scale
+        self.drawingView.loadImage(chatchimage)
+    }
+    internal func OnClickSerchButton(sender:UIButton){
+         self.presentViewController(self.browser, animated: true, completion: nil)
+    }
+   
+    func browserViewControllerDidFinish(
+        browserViewController: MCBrowserViewController!)  {
+        // Called when the browser view controller is dismissed (ie the Done
+        // button was tapped)
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        browserViewController: MCBrowserViewController!)  {
+        // Called when the browser view controller is cancelled
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func session(session: MCSession!, didReceiveData data: NSData!,
+                 fromPeer peerID: MCPeerID!)  {
+        // Called when a peer sends an NSData to us
+        
+        // This needs to run on the main queue
+        dispatch_async(dispatch_get_main_queue()) {
+            
+            
+            
+            self.updateimg(data!)
+        }
+    }
+    
+    // The following methods do nothing, but the MCSessionDelegate protocol
+    // requires that we implement them.
+    func session(session: MCSession!,
+                 didStartReceivingResourceWithName resourceName: String!,
+                                                   fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)  {
+        
+        // Called when a peer starts sending a file to us
+    }
+    
+    func session(session: MCSession!,
+                 didFinishReceivingResourceWithName resourceName: String!,
+                                                    fromPeer peerID: MCPeerID!,
+                                                             atURL localURL: NSURL!, withError error: NSError!)  {
+        // Called when a file has finished transferring from another peer
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!,
+                 withName streamName: String!, fromPeer peerID: MCPeerID!)  {
+        // Called when a peer establishes a stream with us
+    }
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!,
+                 didChangeState state: MCSessionState)  {
+        // Called when a connected peer changes state (for example, goes offline)
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
